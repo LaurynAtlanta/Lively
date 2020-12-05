882e216610fa0d1e2977c47e29ccb8ad452aac75
“Lauryn <laurynmills111@gmail.com>
2020-12-05 15:20:52 -0500

    navigation bar mobile first

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import classes from './Nav.module.scss';
 import logo from '../../images/logo.svg';
 
 const Nav = () => {
+    const [clicked, setClicked] = useState(false);
+    const toggleHandler= () => {
+        setClicked(!clicked);
+    }
+
+
     return (  
         <nav>
-            <div className={classes.burger}>
+            <ul className={clicked? `${classes.show} ${classes.left}`: classes.left}>
+                <li>WOMEN</li>
+                <li>MEN</li>
+            </ul>
+            <div className={classes.burger} onClick={toggleHandler}>
                 <div className={classes.navline}></div>
                 <div className={classes.navline}></div>
                 <div className={classes.navline}></div>
@@ -13,6 +23,10 @@ const Nav = () => {
             <div className={classes.logo}>
                 <img src={logo} alt="Logo"/>
             </div>
+            <ul className={clicked? `${classes.show} ${classes.right}`: classes.right}>
+                <li>ABOUT</li>
+                <li>CONTACT</li>
+            </ul>
         </nav>
     );
 }
